e4079311e8ece6d53fef11d3c6ce49168f59ff0d
INVICTO-RD <geraldinopenalo@gmail.com>
2025-01-11 17:31:01 -0400

    Update menu-main.js

--- plugins/menu-main.js
+++ plugins/menu-main.js
@@ -1,1 +1,35 @@
+// menu-main.js
 
+function showMenu() {
+    const commands = [
+        { command: '.help', description: 'Muestra la lista de comandos disponibles' },
+        { command: '.info', description: 'Proporciona información sobre el bot' },
+        { command: '.exit', description: 'Cierra el menú' }
+    ];
+
+    const menuHeader = `
+        <div style="text-align: center;">
+            <img src="URL_DE_LA_FOTO" alt="Foto de perfil" style="width: 100px; height: 100px;">
+            <h2>Menú de Comandos</h2>
+        </div>
+    `;
+
+    const menuBody = commands.map(cmd => `
+        <p><strong>${cmd.command}</strong> - ${cmd.description}</p>
+    `).join('');
+
+    const menu = `
+        <div style="border: 1px solid black; padding: 10px;">
+            ${menuHeader}
+            ${menuBody}
+        </div>
+    `;
+
+    console.log(menu);
+}
+
+// Ejemplo de cómo se llamaría a la función showMenu cuando se escribe .menu
+const input = '.menu';
+if (input === '.menu') {
+    showMenu();
+}
